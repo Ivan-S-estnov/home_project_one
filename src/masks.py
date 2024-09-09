c985e3191a46b65599ac8a59c111259d978feb91
Ivan <jaloylee@yandex.ru>
2024-09-09 08:59:33 +0400

    masks.py fixes

--- src/masks.py
+++ src/masks.py
@@ -1,6 +1,6 @@
 def get_mask_card_number(card_numb: str) -> str:
     """Функция принимает на вход номер карты и возвращает ее маску"""
-    return f"{card_numb[:4]}{card_numb[4:6]}** ****{card_numb[-4:]}"
+    return f"{card_numb[:4]} {card_numb[4:6]}** **** {card_numb[-4:]}"
 
 
 print(get_mask_card_number("7000792289606361"))
@@ -8,7 +8,7 @@ print(get_mask_card_number("7000792289606361"))
 
 def get_mask_account(mask_account: str) -> str:
     """Функция принимает на вход номер счета и возвращает его маску"""
-    return f"** {mask_account[-4:]}"
+    return f"**{mask_account[-4:]}"
 
 
 print(get_mask_account("73654108430135874305"))
